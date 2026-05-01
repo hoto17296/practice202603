5fc7d3e9a2a92f1ccc8e84e0c15fd246162fe84f
Yuki Ishikawa <hoto@y32.jp>
2026-05-01 02:19:46 +0000

    lib.session

---
+++ backend/lib/session.py
@@ -0,0 +1,46 @@
+import uuid
+from typing import TypedDict, cast
+
+import jwt
+from fastapi import HTTPException, Request, Response
+from sqlmodel import select
+
+from database import get_database_session
+from settings import settings
+from tables import UserTable
+
+JWT_SIGN_ALGORITHM = "HS256"
+
+
+class JwtPayload(TypedDict):
+    id: str
+
+
+class AuthSession(TypedDict):
+    user: UserTable
+
+
+async def get_session(request: Request) -> AuthSession:
+    token = request.cookies.get(settings.session_key)
+    if token is None:
+        raise HTTPException(status_code=401)
+    try:
+        payload = cast(JwtPayload, jwt.decode(token, settings.jwt_private_key, JWT_SIGN_ALGORITHM))
+    except jwt.InvalidTokenError:
+        raise HTTPException(status_code=401)
+    async with get_database_session() as db:
+        stmt = select(UserTable).where(UserTable.id == uuid.UUID(payload["id"]))
+        user = (await db.exec(stmt)).first()
+    if user is None:
+        raise HTTPException(status_code=401)
+    return {"user": user}
+
+
+def set_session(response: Response, user: UserTable):
+    payload: JwtPayload = {"id": str(user.id)}
+    token = jwt.encode(dict(payload), settings.jwt_private_key, JWT_SIGN_ALGORITHM)
+    response.set_cookie(settings.session_key, token, secure=True, httponly=True)
+
+
+def clear_session(response: Response):
+    response.delete_cookie(settings.session_key)
--- backend/settings.py
+++ backend/settings.py
@@ -4,6 +4,8 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     debug: bool = False
     password_pepper: str
+    session_key: str = "session"
+    jwt_private_key: str
 
 
 settings = Settings()  # pyright: ignore[reportCallIssue]
